f0023749cf945b1bf1f8828797af80f3f65cbb32
Snehitha Gulla <SnehithaG@amzurtech.com>
2023-07-17 14:49:30 +0530

    my second commit

--- Memoir/src/app/app.module.ts
+++ Memoir/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { MakeupComponent } from './makeup/makeup.component';
+import { SkincareComponent } from './skincare/skincare.component';
+import { HaircareComponent } from './haircare/haircare.component';
+import { MyspaceComponent } from './myspace/myspace.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    MakeupComponent,
+    SkincareComponent,
+    HaircareComponent,
+    MyspaceComponent
   ],
   imports: [
     BrowserModule,
